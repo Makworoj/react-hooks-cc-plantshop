a2bf5b1e0759700d814331413574dbe71dbdb520
Makworoj <makworomokaya@gmail.com>
2025-11-08 16:41:53 +0300

    almost done

--- src/components/NewPlantForm.js
+++ src/components/NewPlantForm.js
@@ -1,10 +1,27 @@
-import React from "react";
+function NewPlantForm({ onAddPlant }) {
+  function handleSubmit(e) {
+    e.preventDefault();
+    const formData = {
+      name: e.target.name.value,
+      image: e.target.image.value,
+      price: e.target.price.value,
+    };
 
-function NewPlantForm() {
+    fetch("http://localhost:6001/plants", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(formData),
+    })
+      .then((r) => r.json())
+      .then((newPlant) => onAddPlant(newPlant));
+
+    e.target.reset();
+  }
+
   return (
     <div className="new-plant-form">
       <h2>New Plant</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input type="text" name="name" placeholder="Plant name" />
         <input type="text" name="image" placeholder="Image URL" />
         <input type="number" name="price" step="0.01" placeholder="Price" />
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,16 +1,35 @@
-import React from "react";
-import NewPlantForm from "./NewPlantForm";
+import React, { useEffect, useState } from "react";
 import PlantList from "./PlantList";
+import NewPlantForm from "./NewPlantForm";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([]);
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then((res) => res.json())
+      .then((data) => setPlants(data))
+      .catch((err) => console.error("Fetch Error:", err));
+  }, []);
+
+  const filteredPlants = plants.filter((plant) =>
+    plant.name.toLowerCase().includes(search.toLowerCase())
+  );
+
+  function handleAddPlant(newPlant) {
+    setPlants([...plants, newPlant]);
+  }
+
   return (
-    <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
-    </main>
+    <>
+      <NewPlantForm onAddPlant={handleAddPlant} />
+      <Search onSearch={setSearch} />
+      <PlantList plants={filteredPlants} />
+    </>
   );
 }
 
 export default PlantPage;
+
